735dfb5cc6903db50cf49d8080d06d8541b658b7
Ankit Mukherjee <103871482+minimilist@users.noreply.github.com>
2023-02-03 11:37:59 +0530

    Update control.py
    Providing the expression for torque that would provide control to the simulation

--- control.py
+++ control.py
@@ -15,7 +15,9 @@ def controller(q1, q2, w1, w2, q1_desired, q2_desired):
     t2: The torques required for the motor 2 for the current state
     '''
     # edit the code given below
-    t1 = 0
-    t2 = 0
+    a1 = 990*(q1_desired - q1) - 200*w1
+    a2 = 990*(q2_desired - q2) - 200*w2
+    t1 = (3 + 2*math.cos(q2))*a1 + (1 + math.cos(q2))*a2 + 0.5*(2*math.sin(q2)*w1*w2 + math.sin(q2)*w2*w2) + 0.5*9.8*(2*math.sin(q1) + math.sin(q1 + q2))
+    t2 = (1 + math.cos(q2))*a1 + a2 - 0.5*math.sin(q2)*w1*w1 + 0.5*9.8*math.sin(q1 + q2)
 
     return t1, t2
